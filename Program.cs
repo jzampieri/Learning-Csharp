04e2a959be5a8d2ddc39bf76e92f2ecc97f205b2
Julio <juliocezampieri@gmail.com>
2024-03-14 01:42:58 -0300

    Average Method - Curse Conclusion

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ string mensagemDeBoasVindas = "Boas vindas ao meu programa!";
 //List<string> listaDasBandas = new List<string>{"U2", "The Beatles", "Calipso", "Iron Maiden"};
 
 Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
-bandasRegistradas.Add("Link Park", new List<int> { 10, 8, 6});
+bandasRegistradas.Add("Linkin Park", new List<int> { 10, 8, 6});
 bandasRegistradas.Add("The Beatles", new List<int>());
 
 void ExibirLogo()
@@ -86,11 +86,14 @@ void AvaliarUmaBanda()
     Console.Write("Digite o nome da banda que deseja avaliar");
     string nomeDaBanda = Console.ReadLine()!;
     if (bandasRegistradas.ContainsKey(nomeDaBanda)) // ContainsKey verifica se existe a chave pedida dentro do dicionario
-       Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
-       int nota = int.Parse( Console.ReadLine()!);
-       bandasRegistradas[nomeDaBanda].Add(nota);
     {
-
+        Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
+        int nota = int.Parse( Console.ReadLine()!);
+        bandasRegistradas[nomeDaBanda].Add(nota);
+        Console.WriteLine($"\nA nota {nota} foi registrada com sucesso!");
+        Thread.Sleep(2000);
+        Console.Clear();
+        ExibirOpçoesDoMenu();
     }else
     {
         Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
